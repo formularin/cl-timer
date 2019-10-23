f577354716480ff8a0f519dfb9a074d3a9b95c78
lol-cubes <arinmkhare@gmail.com>
2019-10-22 21:14:24 -0400

    remove unecessary slicing in art.py

--- cl-timer/art.py
+++ cl-timer/art.py
@@ -5,7 +5,7 @@ TITLE_ART = '\
              .    Timer\n    #                         .\n     #              \
          .\n     #                       .\n      #                     .\n   \
      # #            .  .\n            #  #  #  # \n\n\npress any key to contin\
-ue\n'[1:]
+ue\n'
 
 
 DIGITS = [
@@ -24,7 +24,7 @@ DIGITS = [
 DISCLAIMER = '\
 This is a beta version, so the displayed time\nwill not be the same as the st\
 ored ones.\n\nKnow that the stored times are always correct,\ntherefore your a\
-verages are always accurate.\n\nPress any key to continue.'[1:]
+verages are always accurate.\n\nPress any key to continue.'
 
 
 DECIMAL_POINT = ' \n \n.\n '
@@ -35,7 +35,7 @@ TIMER_BACKGROUND = '\
                                           |\n|                                \
               |\n|                                              |\n|          \
                                     |\n---------------------------------------\
----------'[1:]
+---------'
 
 
 STARTING_TIME = ' __     __   __\n|  |   |  | |  |\n|__| . |__| |__|'
