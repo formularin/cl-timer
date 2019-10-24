6adfb55d21a49a11375222d19dbee0502ab6e14a
lol-cubes <arinmkhare@gmail.com>
2019-10-24 19:42:17 -0400

    added more stats

--- cl-timer/timer.py
+++ cl-timer/timer.py
@@ -9,6 +9,11 @@ from graphics import Canvas, Cursor, Image, InputLine, NumberDisplay, Char
 from scramble import generate_scramble
 
 
+import logging
+
+logging.basicConfig(filename='cl-timer.log', level=logging.INFO)
+
+
 char = lambda string: Char.fromstring(string)
 
 HOME = f'/Users/{getpass.getuser()}'
@@ -82,10 +87,13 @@ def add_zero(number):
     Add a zero if the value doesn't have
     2 digits behind it's decimal point
     """
-    list_number = list(str(number))
-    if len(list_number[list_number.index('.'):]) < 2:
-        list_number.append('0')
-    return ''.join(list_number)
+    if number == '':
+        return ''
+    else:
+        list_number = list(str(number))
+        if len(list_number[list_number.index('.') + 1:]) < 2:
+            list_number.append('0')
+        return ''.join(list_number)
 
 
 def main(stdscr):
@@ -94,6 +102,8 @@ def main(stdscr):
     """
 
     times = []
+    ao5s = []
+    ao12s = []
     session_file = ""
     def signal_handler(sig, frame):
         """
@@ -103,12 +113,10 @@ def main(stdscr):
         (saving file interaction to the end saves time during frames.)
         """
         if times != [] and session_file != "":
-            list_string_times = [list(str(time)) for time in times]
-            for time in list_string_times:
-                time = add_zero(time)
-            string_of_times = '\n'.join(''.join(time) for time in list_string_times)
+            lines = ['\t'.join([t, a5, a12]) for t, a5, a12 in zip(
+                *[[add_zero(i) for i in lst] for lst in [times, ao5s, ao12s]])]
             with open(session_file, 'w') as f:
-                f.write(string_of_times)
+                f.write('\n'.join(lines))
         raise ExitException()
 
     signal.signal(signal.SIGINT, signal_handler)
@@ -133,42 +141,63 @@ def main(stdscr):
     session_file = f'{HOME}/.cl-timer/{session}'
     
     with open(session_file, 'r') as f:
-        times = [float(i) for i in f.read().split('\n')[1:]]
+        time_lines = [line.split('\t') for line in f.read().split('\n')[:-1]]
+
+    for line in time_lines:
+        times.append(line[0])
+        ao5s.append(line[1])
+        ao12s.append(line[2])
 
     display_text(stdscr, DISCLAIMER)
 
-    def calculate_average(length):
+    def calculate_average(solve, length):
         """
-        Returns current average of `length`
+        Returns average of `length` during `solve`
 
-        Looks through session file and finds last `length` solves.
-        Excludes best and worst times, and return average of the rest.
+        Looks through times list and finds last `length` solves before `solve`
+        Excludes best and worst times, and returns average of the rest.
         """
         if len(times) < length:
             # `length` solves haven't been done yet.
             return ''
         else:
-            latest_average = times[len(times) - length:]  # list of last `length` solves
+            latest_average = times[solve - length:]  # list of last `length` solves
+            latest_average = [float(i) for i in latest_average]
             latest_average.remove(max(latest_average))
             latest_average.remove(min(latest_average))
 
             # calculate average and add zero if it doesn't go to 100ths place.
+            
+            # list of chars of the string version of average rounded to nearest hundredth
             average_chars = list(str(round(sum(latest_average) / (length - 2), 2)))
             if len(average_chars[average_chars.index('.'):]) < 2:
                 average_chars.append('0')
             
             return ''.join(average_chars)
 
+    def get_best_average(length):
+        """
+        Returns best average of `length` in session
+        """
+        try:
+            if length == 5:
+                best = add_zero(min([i for i in ao5s if i != '']))
+            elif length == 12:
+                best = add_zero(min([i for i in ao12s if i != '']))
+        except ValueError:
+            return ""
+        return best
+
     def get_best_time():
         try:
-            best = add_zero(min(times))
+            best = add_zero(min([float(i) for i in times]))
         except ValueError:
             return ""
         return best
 
     def get_worst_time():
         try:
-            worst = add_zero(max(times))
+            worst = add_zero(max([float(i) for i in times]))
         except ValueError:
             return ""
         return worst
@@ -179,10 +208,13 @@ def main(stdscr):
     number_display = NumberDisplay(canvas, 15, 5)
     timer_background = Image(canvas, 0, 3, char(TIMER_BACKGROUND))
 
-    ao5_image = Image(canvas, 51, 4, char(f'AO5: {calculate_average(5)}'))
-    ao12_image = Image(canvas, 51, 5, char(f'AO12: {calculate_average(12)}'))
-    best_time_image = Image(canvas, 51, 6, char(f'Best time: {get_best_time()}'))
-    worst_time_image = Image(canvas, 51, 7, char(f'Worst time: {get_worst_time()}'))
+    ao5_image = Image(canvas, 51, 4, char(f'AO5: {calculate_average(len(times), 5)}'))
+    ao12_image = Image(canvas, 51, 5, char(f'AO12: {calculate_average(len(times), 12)}'))
+    best_ao5_image = Image(canvas, 51, 6, char(f'Best AO5: {get_best_average(5)}'))
+    best_ao12_image = Image(canvas, 51, 7, char(f'Best AO12: {get_best_average(12)}'))
+    best_time_image = Image(canvas, 51, 8, char(f'Best time: {get_best_time()}'))
+    worst_time_image = Image(canvas, 51, 9, char(f'Worst time: {get_worst_time()}'))
+    number_of_times_image = Image(canvas, 51, 10, char(f'Number of Times: {len(times)}'))
 
     timer_running = False
     delay = 0  # how far behind the program is
@@ -214,14 +246,25 @@ def main(stdscr):
                 scramble_image.chars = char(new_scramble)
 
                 # calculate stats and update images on screen
-                ao5 = calculate_average(5)
+                ao5 = calculate_average(len(times), 5)
+                ao5s.append(ao5)
                 ao5_image.chars = char(f'AO5: {ao5}')
-                ao12 = calculate_average(12)
+                ao12 = calculate_average(len(times), 12)
+                ao12s.append(ao12)
                 ao12_image.chars = char(f'AO12: {ao12}')
+                best_ao5 = get_best_average(5)
+                best_ao5_image.chars = char(f'Best AO5: {best_ao5}')
+                best_ao12 = get_best_average(12)
+                best_ao12_image.chars = char(f'Best AO12: {best_ao12}')
                 best_time = get_best_time()
                 best_time_image.chars = char(f'Best time: {best_time}')
                 worst_time = get_worst_time()
                 worst_time_image.chars = char(f'Worst time: {worst_time}')
+                number_of_times_image.chars = char(f'Number of Times: {len(times)}')
+
+                # logging.info(f'{ao5s=}')
+                # logging.info(f'{ao12s=}')
+                # logging.info(f'{times=}')
             else:
                 timer_running = True
                 number_display.reset()
@@ -235,8 +278,11 @@ def main(stdscr):
 
         ao5_image.render()
         ao12_image.render()
+        best_ao5_image.render()
+        best_ao12_image.render()
         best_time_image.render()
         worst_time_image.render()
+        number_of_times_image.render()
 
         stdscr.clear()
         stdscr.addstr(canvas.display)
