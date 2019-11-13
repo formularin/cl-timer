bece6ff6f8662f9d92e914d52fa080cce408002d
lol-cubes <arinmkhare@gmil.com>
2019-11-12 21:00:27 -0500

    increase version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cl-timer",
-    version="1.0.5",
+    version="1.0.6",
     author="Arin Khare",
     author_email="arinmkhare@gmail.com",
     description="A Cubing Timer for the Terminal",
